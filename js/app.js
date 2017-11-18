c90dbed5b9c93ec08144475bbfdd827d75876fe6
valeriavalles <valeria.sv@outlook.es>
2017-11-18 17:24:08 -0500

    Versión 0.0.3/Versión 0.0.4/Versión 0.0.5

--- js/app.js
+++ js/app.js
@@ -1,40 +1,39 @@
-window.addEventListener('load',function(){
-var area = document.getElementById('textarea');
-var boton = document.getElementById('save');
-var lista = document.getElementById('container-links');
-var contador = document.getElementById('contador');
-console.log(contador);
-
-boton.addEventListener('click', function(event) {
-  if (area.value) {
-    var li = document.createElement('li');
-    var link = document.createElement('p');
-    link.textContent = area.value;
-    
-    li.appendChild(link);
-    lista.appendChild(li);
-    area.value = ''; 
-  
-  }
-});
- area.addEventListener('keyup', habilitar)
-function habilitar() {
-  if(area.value!= ""&&area.value.length>0){
-    boton.disabled = false;
-  }else{
-    boton.disabled = true;
-  }  
-};
-
-
-area.addEventListener('keydown', tama);
-
-function tama(){
-  this.style.cssText = 'height:auto; padding:0';
-  this.style.cssText = 'height:' + this.scrollHeight + 'px'
-  contador.textContent = 140 - area.value.length;
-};
-
-//contador.textContent = 140 - area.length ;
-
+window.addEventListener('load', function() {
+/* llamando a los id para los eventos*/ 
+  var area = document.getElementById('textarea');
+  var boton = document.getElementById('save');
+  var lista = document.getElementById('container-links');
+  var contador = document.getElementById('contador');
+  // agregandole evento al boton tweet
+  boton.addEventListener('click', function(event) {
+    if (area.value) {
+      var li = document.createElement('li');
+      var link = document.createElement('p');
+      link.textContent = area.value;
+      li.appendChild(link);
+      lista.appendChild(li);
+      area.value = '';
+    }
+  });
+  // habilitando y desabilitando el boton tweet
+  area.addEventListener('keyup', habilitar);
+  function habilitar() {
+    if (area.value != ' ' && (area.value.length > 0) && area.value.length <= 140) {
+      boton.disabled = false;
+    } else {
+      boton.disabled = true;
+    } 
+    if (area.value.length > 120) {
+      contador.classList.add('nearly');
+    }
+    if (area.value.length > 130) {
+      contador.classList.add('very-nearly');
+    }
+  };
+  area.addEventListener('keydown', tama);
+  function tama() {
+    this.style.cssText = 'height:auto; padding:0';
+    this.style.cssText = 'height:' + this.scrollHeight + 'px';
+    contador.textContent = 140 - area.value.length;
+  };
 });   
