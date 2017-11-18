87e4934151b905875fe6fe40267d8e4a657390f4
valeriavalles <valeria.sv@outlook.es>
2017-11-18 10:12:10 -0500

    Versión 0.0.2

--- js/app.js
+++ js/app.js
@@ -2,25 +2,39 @@ window.addEventListener('load',function(){
 var area = document.getElementById('textarea');
 var boton = document.getElementById('save');
 var lista = document.getElementById('container-links');
+var contador = document.getElementById('contador');
+console.log(contador);
 
 boton.addEventListener('click', function(event) {
   if (area.value) {
     var li = document.createElement('li');
     var link = document.createElement('p');
     link.textContent = area.value;
-    //link.setAttribute('href', '#');//
+    
     li.appendChild(link);
     lista.appendChild(li);
-    area.value = '';
+    area.value = ''; 
+  
   }
 });
+ area.addEventListener('keyup', habilitar)
+function habilitar() {
+  if(area.value!= ""&&area.value.length>0){
+    boton.disabled = false;
+  }else{
+    boton.disabled = true;
+  }  
+};
+
+
 area.addEventListener('keydown', tama);
 
 function tama(){
   this.style.cssText = 'height:auto; padding:0';
   this.style.cssText = 'height:' + this.scrollHeight + 'px'
+  contador.textContent = 140 - area.value.length;
 };
 
-
+//contador.textContent = 140 - area.length ;
 
 });   
